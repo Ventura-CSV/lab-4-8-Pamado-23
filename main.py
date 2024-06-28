91dde78758c0705300e8dc561a52fcfa16bfef4c
Pamado-23 <pamado1@my.vcccd.edu>
2024-06-28 13:51:26 -0700

    Lab 4-8

--- main.py
+++ main.py
@@ -4,6 +4,29 @@ def main():
     # Comlete your code here
     ##################################################
 
+
+    begin = int(input(f'First value greater than 1: '))
+
+    end = int(input(f'Second value greater than 1: '))
+
+    if begin <= 1 or end <= 1:
+        print ()
+    elif begin >= end:
+        print()
+    else:
+        for num in range (begin, end + 1):
+            if num > 1:
+                prime = True
+                for i in range(2, int(num**0.5)+1):
+                    if num % i == 0:
+                        prime = False
+                if prime:
+                    plist.append(num)
+        print (' ', begin, ' ', end, ' ', plist)
+    
+
+
+
     return plist
 
 
